6474c67320b1d9e7d1beed259908fda31bb02a40
rodica-andronache <rodik_andronache@yahoo.com>
2015-08-26 18:14:28 +0300

    !!! Show theme version only if variable exists

--- inc/admin/welcome-screen/sections/welcome.php
+++ inc/admin/welcome-screen/sections/welcome.php
@@ -8,7 +8,7 @@ $zerif_lite = wp_get_theme( 'zerif-lite' );
 ?>
 <div class="zerif-lite-tab-pane active" id="welcome">
 	
-	<h1>Zerif Lite <sup id="zerif-lite-theme-version"><?php echo esc_attr( $zerif_lite['Version'] ); ?> </sup></h1>
+	<h1>Zerif Lite <?php if( !empty($zerif_lite['Version']) ): ?> <sup id="zerif-lite-theme-version"><?php echo esc_attr( $zerif_lite['Version'] ); ?> </sup><?php endif; ?></h1>
 
 	<p><?php esc_html_e( 'Welcome to our most popular free one page WordPress theme, Zerif Lite!','zerif-lite'); ?></p>
 	<p><?php esc_html_e( 'We want to make sure you have the best experience using Zerif Lite and that is why we gathered here all the necessary informations for you. We hope you will enjoy using Zerif Lite, as much as we enjoy creating great products.', 'zerif-lite' ); ?>
