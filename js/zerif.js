ce514c5afed6d7a8f763af30ac3ba0cd30a486f9
TonyArchambeau <archambeautony@free.fr>
2015-09-10 16:56:29 +0200

    accessility modification : add some translated sentances + adapt keyboard navigation for child link in menu

--- js/zerif.js
+++ js/zerif.js
@@ -19,8 +19,12 @@ jQuery(window).load(function() {
 
   jQuery('.carousel').carousel('pause');
 
+  // Focus styles for menus.
+  jQuery( '.navbar-collapse' ).find( 'a' ).on( 'focus blur', function() {
+    console.info("apply focus on a");
+    jQuery( this ).parents().toggleClass( 'link-focus' );
+  } );
 })
-
 
 /*** DROPDOWN FOR MOBILE MENU */
 var callback_mobile_dropdown = function () {
