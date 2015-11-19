256f071675d5868430cbd01f48f002945c6b851b
rodica-andronache <rodik_andronache@yahoo.com>
2015-11-19 17:17:36 +0200

    !!! Fixed #245, required actions for choosing latest news

--- functions.php
+++ functions.php
@@ -103,6 +103,11 @@ function zerif_setup() {
                 "title" => esc_html__( 'Check the contact form after installing Pirate Forms' ,'zerif-lite' ),
                 "description"=> esc_html__( "After installing the Pirate Forms plugin, please make sure you check your frontpage contact form is working fine. Also, if you use Zerif Lite in other language(s) please make sure the translation is ok. If not, please translate the contact form again.",'zerif-lite' ),
             ),
+			array(
+                "id" => 'zerif-lite-req-ac-frontpage-latest-news',
+                "title" => esc_html__( 'Get the one page template' ,'zerif-lite' ),
+                "description"=> esc_html__( 'If you just installed Zerif Lite, and are not able to see the one page template, you need to go to Settings -> Reading , Front page displays and select "Your latest posts".','zerif-lite' )
+            ),
 
         );
 
