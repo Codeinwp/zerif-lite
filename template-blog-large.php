42aab4354bb1f6a954781cefa2744f195b24c471
Cristian Ungureanu <cristian@themeisle.com>
2016-08-25 19:58:51 +0300

    #355 - fix for post navigation links when frontpage have blog template

--- template-blog-large.php
+++ template-blog-large.php
@@ -22,27 +22,28 @@ global $paged;?>
 
 					<?php
 					// Define custom query parameters
-					$custom_query_args = array(
+					$zerif_posts_per_page = get_option('posts_per_page');
+					$zerif_custom_query_args = array(
 						/* Parameters go here */
 						'post_type' => 'post',
-						'posts_per_page' => 6 );
+						'posts_per_page' => $zerif_posts_per_page );
 
 					// Get current page and append to custom query parameters array
-					$custom_query_args['paged'] = ( get_query_var('paged') ? get_query_var('paged') : ( get_query_var('page') ? get_query_var('page') : 1) );
-					$paged = $custom_query_args['paged'];
+					$zerif_custom_query_args['paged'] = ( get_query_var('paged') ? get_query_var('paged') : ( get_query_var('page') ? get_query_var('page') : 1) );
+					$paged = $zerif_custom_query_args['paged'];
 
 					// Instantiate custom query
-					$custom_query = new WP_Query( apply_filters( 'zerif_template_blog_large_parameters', $custom_query_args ) );
+					$zerif_custom_query = new WP_Query( apply_filters( 'zerif_template_blog_large_parameters', $zerif_custom_query_args ) );
 
 					// Pagination fix
-					$temp_query = $wp_query;
+					$zerif_temp_query = $wp_query;
 					$wp_query   = NULL;
-					$wp_query   = $custom_query;
+					$wp_query   = $zerif_custom_query;
 
 					// Output custom query loop
-					if ( $custom_query->have_posts() ) :
-						while ( $custom_query->have_posts() ) :
-							$custom_query->the_post();
+					if ( $zerif_custom_query->have_posts() ) :
+						while ( $zerif_custom_query->have_posts() ) :
+							$zerif_custom_query->the_post();
 							// Loop output goes here
 							get_template_part( 'content-large' );
 						endwhile;
@@ -53,11 +54,11 @@ global $paged;?>
 					wp_reset_postdata();
 
 					// Custom query loop pagination
-					zerif_paging_nav($custom_query->max_num_pages);
+					zerif_paging_nav($zerif_custom_query->max_num_pages);
 
 					// Reset main query object
 					$wp_query = NULL;
-					$wp_query = $temp_query; ?>
+					$wp_query = $zerif_temp_query; ?>
 
 				</main><!-- #main -->
 
