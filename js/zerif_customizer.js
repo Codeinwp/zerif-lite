bf7baa053216928fbae049b8cb7c8fa6844b3bea
abaicus <baicusandrei@me.com>
2016-08-30 19:31:06 +0300

    #395. Removed JS injection for customizer links added by extending wp_customizer_section class.

--- js/zerif_customizer.js
+++ js/zerif_customizer.js
@@ -1,21 +1,5 @@
 jQuery(document).ready(function() {
-
-	/* Upsells in customizer (Documentation link and Upgrade to PRO link */
-	if( !jQuery( ".zerif-upsells" ).length ) {
-		jQuery('#customize-theme-controls > ul').prepend('<li class="accordion-section zerif-upsells">');
-	}
-
-	if( jQuery( ".zerif-upsells" ).length ) {
-
-		jQuery('.zerif-upsells').append('<a style="width: 80%; margin: 5px auto 5px auto; display: block; text-align: center;" href="http://themeisle.com/documentation-zerif-lite" class="button" target="_blank">{documentation}</a>'.replace('{documentation}', zerifLiteCustomizerObject.documentation));
-
-	}
-	jQuery('.preview-notice').append('<a class="zerif-upgrade-to-pro-button" href="http://themeisle.com/themes/zerif-pro-one-page-wordpress-theme/" class="button" target="_blank">{pro}</a>'.replace('{pro}',zerifLiteCustomizerObject.pro));
-
-	if ( !jQuery( ".zerif-upsells" ).length ) {
-		jQuery('#customize-theme-controls > ul').prepend('</li>');
-	}
-
+	
 	jQuery( '.ui-state-default' ).on( 'mousedown', function() {
 		jQuery( '#customize-header-actions #save' ).trigger( 'click' );
 
