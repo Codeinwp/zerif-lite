e71403d094b04709831660d8cbb931601f0e475f
rodica-andronache <rodik_andronache@yahoo.com>
2016-08-30 16:19:47 +0300

    #390 use core functions for archive title and description

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -241,91 +241,14 @@ function zerif_page_header_function() {
 }
 
 function zerif_page_header_title_archive_function() {
-	?>
-	<h1 class="page-title">
 
-		<?php
+	the_archive_title( '<h1 class="page-title">', '</h1>' );
 
-		if ( is_category() ) :
-
-			single_cat_title();
-
-		elseif ( is_tag() ) :
-
-			single_tag_title();
-
-		elseif ( is_author() ) :
-
-			printf( __( 'Author: %s', 'zerif-lite' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-		elseif ( is_day() ) :
-
-			printf( __( 'Day: %s', 'zerif-lite' ), '<span>' . get_the_date() . '</span>' );
-
-		elseif ( is_month() ) :
-
-			printf( __( 'Month: %s', 'zerif-lite' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'zerif-lite' ) ) . '</span>' );
-
-		elseif ( is_year() ) :
-
-			printf( __( 'Year: %s', 'zerif-lite' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'zerif-lite' ) ) . '</span>' );
-
-		elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-
-			_e( 'Asides', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-
-			_e( 'Galleries', 'zerif-lite');
-
-		elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-
-			_e( 'Images', 'zerif-lite');
-
-		elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-
-			_e( 'Videos', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-
-			_e( 'Quotes', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-
-			_e( 'Links', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-
-			_e( 'Statuses', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-
-			_e( 'Audios', 'zerif-lite' );
-
-		elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-
-			_e( 'Chats', 'zerif-lite' );
-
-		else :
-
-			_e( 'Archives', 'zerif-lite' );
-
-		endif;
-
-		?>
-
-	</h1>
-	<?php
 }
 
 function zerif_page_term_description_archive_function() {
-	$term_description = term_description();
 
-	if ( ! empty( $term_description ) ) :
-
-		printf( '<div class="taxonomy-description">%s</div>', $term_description );
-
-	endif;
+	the_archive_description( '<div class="taxonomy-description">', '</div>' );
 }
 
 function zerif_footer_widgets_function() {
