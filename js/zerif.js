294a455adb02f0fed6405c3d2f7d9cc26801b919
Cristian Ungureanu <cristian@themeisle.com>
2016-10-06 15:02:10 +0300

    #426 - add outline for slider buttons only at tab focus

--- js/zerif.js
+++ js/zerif.js
@@ -24,6 +24,15 @@ jQuery(window).load(function() {
         jQuery( this ).parents().toggleClass( 'link-focus' );
     });
 
+    jQuery( '#carousel-homepage-latestnews' ).find('.carousel-control').on( 'keyup', function(e) {
+        var code = e.keyCode || e.which;
+        jQuery( '.carousel-control' ).removeClass('link-focus');
+        if (code == '9') {
+            jQuery( this ).toggleClass( 'link-focus' );
+            return false;
+        }
+    });
+
 });
 
 /*** DROPDOWN FOR MOBILE MENU */
