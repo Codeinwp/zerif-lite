5b4477e02d2af6ea2897a00d6c3218de0e5ca38f
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2016-10-30 21:22:38 +0200

    #445 fix theme info section

--- inc/class/class-zerif-theme-info.php
+++ inc/class/class-zerif-theme-info.php
@@ -2,22 +2,22 @@
 class Zerif_Theme_Info extends WP_Customize_Control {
 
 	public function render_content() {
-		$docs_url = 'http://docs.themeisle.com/article/421-shop-isle-documentation-wordpress-org';
+		$docs_url = 'http://docs.themeisle.com/article/5-zerif-lite-documentation';
 		$demo_url = 'http://demo.themeisle.com/zerif-lite/';
 		$fvp_url = 'http://docs.themeisle.com/article/65-zerif-lite-versus-zerif-pro';
 		$review_url = 'https://wordpress.org/support/view/theme-reviews/zerif-lite'; ?>
 		<div class="zerif-theme-info">
 			<?php
-			echo sprintf( wp_kses( __( ' <a href="%s">View Documentation</a>', 'zerif-lite' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $docs_url ) ); ?>
+			printf( '<a href="'.esc_url( $docs_url ).'" target="_blank">%s</a>', __( 'View Documentation', 'zerif-lite' ) ); ?>
 			<hr/>
 			<?php
-			echo sprintf( wp_kses( __( ' <a href="%s">View Demo</a>', 'zerif-lite' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $demo_url ) ); ?>
+			printf( '<a href="'.esc_url( $demo_url ).'" target="_blank">%s</a>', __( 'View Demo', 'zerif-lite' ) ); ?>
 			<hr/>
 			<?php
-			echo sprintf( wp_kses( __( ' <a href="%s">Free VS Pro</a>', 'zerif-lite' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $fvp_url ) ); ?>
+			printf( '<a href="'.esc_url( $fvp_url ).'" target="_blank">%s</a>', __( 'Free VS Pro', 'zerif-lite' ) ); ?>
 			<hr/>
 			<?php
-			echo sprintf( wp_kses( __( ' <a href="%s">Leave a review</a>', 'zerif-lite' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $review_url ) ); ?>
+			printf( '<a href="'.esc_url( $review_url ).'" target="_blank">%s</a>', __( 'Leave a review', 'zerif-lite' ) ); ?>
 		</div>
 		<?php
 	}
