54afff946023c2f9be6440d1105d59ac5bd32c32
Andrei Băicuș <baicusandrei@me.com>
2016-11-03 10:40:47 +0200

    #441. Added missing bracket.

--- inc/customizer.php
+++ inc/customizer.php
@@ -1951,14 +1951,16 @@ add_action( 'customize_controls_enqueue_scripts', 'zerif_late_registers', 99 );
  */
 function zerif_custom_logo_callback() {
 	if ( get_theme_mod( 'custom_logo' ) ) {
-	    $logo = '';
-        $zerif_custom_logo = get_theme_mod( 'custom_logo' );
+		$logo              = '';
+		$zerif_custom_logo = get_theme_mod( 'custom_logo' );
 
-        if ( !empty( $zerif_custom_logo ) ) {
-            $custom_logo = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
-		$logo = '<a href="' . esc_url( home_url( '/' ) ) . '"><img src="' . esc_url( $custom_logo[0] ) . '"></a>';
-	} else {
-		$logo = '<div class="site-title-tagline-wrapper"><h1 class="site-title"><a href="'. esc_url( home_url( '/' ) ) .'">' . get_bloginfo( 'name' ) . '</a></h1><p class="site-description">' . get_bloginfo('description') . '</p></div>';
+		if ( ! empty( $zerif_custom_logo ) ) {
+			$custom_logo = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+			$logo        = '<a href="' . esc_url( home_url( '/' ) ) . '"><img src="' . esc_url( $custom_logo[0] ) . '"></a>';
+		} else {
+			$logo = '<div class="site-title-tagline-wrapper"><h1 class="site-title"><a href="' . esc_url( home_url( '/' ) ) . '">' . get_bloginfo( 'name' ) . '</a></h1><p class="site-description">' . get_bloginfo( 'description' ) . '</p></div>';
+		}
+
+		return $logo;
 	}
-	return $logo;
 }
