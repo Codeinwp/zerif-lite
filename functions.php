95360c0e1ef5688b853297e4b0e71883af339bcc
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2016-11-03 17:11:24 +0200

    #448 small fix for background on homepage

--- functions.php
+++ functions.php
@@ -1469,7 +1469,7 @@ function recaptcha_scripts() {
 add_filter( 'body_class', 'remove_class_function' );
 function remove_class_function( $classes ) {
 
-    if ( !is_home() ) {   
+    if ( !is_home() && !is_page_template('template-frontpage.php') ) {
         // index of custom-background
         $key = array_search('custom-background', $classes);
         // remove class
