73b2f1defd4d7a3cf3c8c594a7b4bcc20bbe61c9
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-01-01 18:11:09 +0200

    #493

--- inc/extras.php
+++ inc/extras.php
@@ -1,1 +1,45 @@
-<?php/** * Custom functions that act independently of the theme templates * Eventually, some of the functionality here could be replaced by core features * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link. */function zerif_page_menu_args( $args ) {	$args['show_home'] = true;	return $args;}add_filter( 'wp_page_menu_args', 'zerif_page_menu_args' );/** * Adds custom classes to the array of body classes. * * @param array $classes Classes for the body element. * @return array */function zerif_body_classes( $classes ) {	// Adds a class of group-blog to blogs with more than 1 published author.	if ( is_multi_author() ) {		$classes[] = 'group-blog';	}	return $classes;}add_filter( 'body_class', 'zerif_body_classes' );
+<?php
+
+/**
+ * Custom functions that act independently of the theme templates
+ * Eventually, some of the functionality here could be replaced by core features
+ * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
+ */
+
+function zerif_page_menu_args( $args ) {
+
+	$args['show_home'] = true;
+
+	return $args;
+
+}
+
+add_filter( 'wp_page_menu_args', 'zerif_page_menu_args' );
+
+/**
+
+ * Adds custom classes to the array of body classes.
+
+ *
+
+ * @param array $classes Classes for the body element.
+
+ * @return array
+
+ */
+
+function zerif_body_classes( $classes ) {
+
+	// Adds a class of group-blog to blogs with more than 1 published author.
+
+	if ( is_multi_author() ) {
+
+		$classes[] = 'group-blog';
+
+	}
+
+	return $classes;
+
+}
+
+add_filter( 'body_class', 'zerif_body_classes' );
