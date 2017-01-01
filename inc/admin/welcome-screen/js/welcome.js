263c3b13f89b67d3f7acf0f9da7b33d69e5c4e85
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-01-01 20:28:38 +0200

    Remove console.log occurance

--- inc/admin/welcome-screen/js/welcome.js
+++ inc/admin/welcome-screen/js/welcome.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function() {
     jQuery(".zerif-dismiss-required-action").click(function(){
 
         var id= jQuery(this).attr('id');
-        console.log(id);
+
         jQuery.ajax({
             type       : "GET",
             data       : { action: 'zerif_lite_dismiss_required_action',dismiss_id : id },
