3ab7a428a89d4ebf54484d1764b78ef124803a39
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-02-28 18:09:15 +0200

    Test encoding

--- sections/our_focus.php
+++ sections/our_focus.php
@@ -1,1 +1,53 @@
-<?php zerif_before_our_focus_trigger(); ?><section class="focus" id="focus">	<?php zerif_top_our_focus_trigger(); ?>	<div class="container">		<!-- SECTION HEADER -->		<div class="section-header">			<!-- SECTION TITLE AND SUBTITLE -->			<?php			/* Title */			zerif_our_focus_header_title_trigger();			/* Subtitle */			zerif_our_focus_header_subtitle_trigger();			?>		</div>		<div class="row">				<?php				if ( is_active_sidebar( 'sidebar-ourfocus' ) ) {					dynamic_sidebar( 'sidebar-ourfocus' );				} elseif ( current_user_can( 'edit_theme_options' ) ) {					if ( is_customize_preview() ) {						printf( __( 'You need to install the %s plugin to be able to add Team members, Testimonials, Our Focus and Clients widgets.','zerif-lite' ), 'ThemeIsle Companion' );					} else {						printf( __( 'You need to install the %s plugin to be able to add Team members, Testimonials, Our Focus and Clients widgets.','zerif-lite' ), sprintf( '<a href="%1$s">%2$s</a>', esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=themeisle-companion' ), 'install-plugin_themeisle-companion' ) ), 'ThemeIsle Companion' ) );					}				}				?>		</div>	</div> <!-- / END CONTAINER -->	<?php zerif_bottom_our_focus_trigger(); ?></section>  <!-- / END FOCUS SECTION --><?php zerif_after_our_focus_trigger(); ?>
+<?php zerif_before_our_focus_trigger(); ?>
+
+<section class="focus" id="focus">
+
+	<?php zerif_top_our_focus_trigger(); ?>
+
+	<div class="container">
+
+		<!-- SECTION HEADER -->
+
+		<div class="section-header">
+
+			<!-- SECTION TITLE AND SUBTITLE -->
+
+			<?php
+
+			/* Title */
+			zerif_our_focus_header_title_trigger();
+
+			/* Subtitle */
+			zerif_our_focus_header_subtitle_trigger();
+
+			?>
+
+		</div>
+
+		<div class="row">
+
+				<?php
+				if ( is_active_sidebar( 'sidebar-ourfocus' ) ) {
+
+					dynamic_sidebar( 'sidebar-ourfocus' );
+
+				} elseif ( current_user_can( 'edit_theme_options' ) ) {
+
+					if ( is_customize_preview() ) {
+						printf( __( 'You need to install the %s plugin to be able to add Team members, Testimonials, Our Focus and Clients widgets.','zerif-lite' ), 'ThemeIsle Companion' );
+					} else {
+						printf( __( 'You need to install the %s plugin to be able to add Team members, Testimonials, Our Focus and Clients widgets.','zerif-lite' ), sprintf( '<a href="%1$s">%2$s</a>', esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=themeisle-companion' ), 'install-plugin_themeisle-companion' ) ), 'ThemeIsle Companion' ) );
+					}
+
+				}
+				?>
+
+		</div>
+
+	</div> <!-- / END CONTAINER -->
+
+	<?php zerif_bottom_our_focus_trigger(); ?>
+
+</section>  <!-- / END FOCUS SECTION -->
+
+<?php zerif_after_our_focus_trigger(); ?>
