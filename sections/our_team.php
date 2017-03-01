31c09c5574b54d828042b07cea7b249a6303cf15
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-03-01 17:59:59 +0200

    Fixed encoding issues in the our team file

--- sections/our_team.php
+++ sections/our_team.php
@@ -1,1 +1,42 @@
-<?phpzerif_before_our_team_trigger();echo '<section class="our-team" id="team">';	zerif_top_our_team_trigger();	echo '<div class="container">';		echo '<div class="section-header">';			/* Title */			zerif_our_team_header_title_trigger();			/* Subtitle */			zerif_our_team_header_subtitle_trigger();		echo '</div>';		if( is_active_sidebar( 'sidebar-ourteam' ) ) {			echo '<div class="row" data-scrollreveal="enter left after 0s over 0.1s">';				dynamic_sidebar( 'sidebar-ourteam' );			echo '</div> ';		} elseif ( current_user_can( 'edit_theme_options' ) ) {			if ( is_customize_preview() ) {				printf( __( 'Add widgets in this area by going to the %s','zerif-lite' ), __( 'Our team section','zerif-lite' ) );			} else {				printf( __( 'Add widgets in this area by going to the %s','zerif-lite' ), sprintf( '<a href="%1$s" class="zerif-default-links">%2$s</a>', esc_url( admin_url( 'customize.php?autofocus&#91;section&#93;=sidebar-widgets-sidebar-ourteam' ) ), __( 'Our team section','zerif-lite' ) ) );			}		}	echo '</div>';	zerif_bottom_our_team_trigger();echo '</section>';zerif_after_our_team_trigger();?>
+<?php
+
+zerif_before_our_team_trigger();
+
+echo '<section class="our-team" id="team">';
+
+	zerif_top_our_team_trigger();
+
+	echo '<div class="container">';
+
+		echo '<div class="section-header">';
+
+			/* Title */
+			zerif_our_team_header_title_trigger();
+
+			/* Subtitle */
+			zerif_our_team_header_subtitle_trigger();
+
+		echo '</div>';
+
+		if( is_active_sidebar( 'sidebar-ourteam' ) ) {
+			echo '<div class="row" data-scrollreveal="enter left after 0s over 0.1s">';
+				dynamic_sidebar( 'sidebar-ourteam' );
+			echo '</div> ';
+		} elseif ( current_user_can( 'edit_theme_options' ) ) {
+
+			if ( is_customize_preview() ) {
+				printf( __( 'Add widgets in this area by going to the %s','zerif-lite' ), __( 'Our team section','zerif-lite' ) );
+			} else {
+				printf( __( 'Add widgets in this area by going to the %s','zerif-lite' ), sprintf( '<a href="%1$s" class="zerif-default-links">%2$s</a>', esc_url( admin_url( 'customize.php?autofocus&#91;section&#93;=sidebar-widgets-sidebar-ourteam' ) ), __( 'Our team section','zerif-lite' ) ) );
+			}
+
+		}
+
+	echo '</div>';
+
+	zerif_bottom_our_team_trigger();
+
+echo '</section>';
+
+zerif_after_our_team_trigger();
+?>
