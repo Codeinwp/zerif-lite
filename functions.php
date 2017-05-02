6a4a62c11e3e307f5a9c549b3ac3ed377ed5c458
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-05-02 22:43:16 +0300

    Added tgmpa back

--- functions.php
+++ functions.php
@@ -78,6 +78,9 @@ function zerif_setup() {
 
 	/* Customizer additions. */
 	require get_template_directory() . '/inc/customizer.php';
+	
+	/* tgm-plugin-activation */
+	require_once get_template_directory() . '/class-tgm-plugin-activation.php';
 
 	/* preview demo */
 	require_once get_template_directory() . '/ti-prevdem/init-prevdem.php';
@@ -696,6 +699,68 @@ function zerif_remove_yoast_rel_link() {
 }
 add_filter( 'wpseo_prev_rel_link', 'zerif_remove_yoast_rel_link' );
 add_filter( 'wpseo_next_rel_link', 'zerif_remove_yoast_rel_link' );
+
+add_action('tgmpa_register', 'zerif_register_required_plugins');
+function zerif_register_required_plugins() {	
+	
+	$wp_version_nr = get_bloginfo('version');
+	
+	if( $wp_version_nr < 3.9 ):
+		$plugins = array(
+			array(
+				'name' => 'Widget customizer',
+				'slug' => 'widget-customizer', 
+				'required' => false 
+			),
+			array(
+				'name'      => 'Pirate Forms',
+				'slug'      => 'pirate-forms',
+				'required'  => false,
+			)
+		);
+		
+	else:
+	
+		$plugins = array(
+			array(
+				'name'      => 'Pirate Forms',
+				'slug'      => 'pirate-forms',
+				'required'  => false,
+			)
+		);
+	
+	endif;
+    $config = array(
+        'default_path' => '',
+        'menu' => 'tgmpa-install-plugins',
+        'has_notices' => true,
+        'dismissable' => true,
+        'dismiss_msg' => '',
+        'is_automatic' => false,
+        'message' => '',
+        'strings' => array(
+            'page_title' => __('Install Required Plugins', 'zerif-lite'),
+            'menu_title' => __('Install Plugins', 'zerif-lite'),
+            'installing' => __('Installing Plugin: %s', 'zerif-lite'),
+            'oops' => __('Something went wrong with the plugin API.', 'zerif-lite'),
+            'notice_can_install_required' => _n_noop('This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.','zerif-lite'),
+            'notice_can_install_recommended' => _n_noop('This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.','zerif-lite'),
+            'notice_cannot_install' => _n_noop('Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.','zerif-lite'),
+            'notice_can_activate_required' => _n_noop('The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.','zerif-lite'),
+            'notice_can_activate_recommended' => _n_noop('The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.','zerif-lite'),
+            'notice_cannot_activate' => _n_noop('Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.','zerif-lite'),
+            'notice_ask_to_update' => _n_noop('The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.','zerif-lite'),
+            'notice_cannot_update' => _n_noop('Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.','zerif-lite'),
+            'install_link' => _n_noop('Begin installing plugin', 'Begin installing plugins','zerif-lite'),
+            'activate_link' => _n_noop('Begin activating plugin', 'Begin activating plugins','zerif-lite'),
+            'return' => __('Return to Required Plugins Installer', 'zerif-lite'),
+            'plugin_activated' => __('Plugin activated successfully.', 'zerif-lite'),
+            'complete' => __('All plugins installed and activated successfully. %s', 'zerif-lite'),
+            'nag_type' => 'updated'
+        )
+    );
+    tgmpa($plugins, $config);
+}
 
 /* Load Jetpack compatibility file. */
 
